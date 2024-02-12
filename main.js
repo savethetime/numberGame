47b210c720994c5438ec26e97a9ba7f92733133f
savethetime <stopwatchstudy@gmail.com>
2024-02-12 23:40:53 +0900

    second commit

--- main.js
+++ main.js
@@ -13,8 +13,15 @@ let computerNum = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 5
+let gameOver = false
+let chanceArea = document.getElementById("chance-area");
+let history=[]
 
 playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", function(){userInput.value=""});
 
 function pickRandomNum() {
     computerNum = Math.floor(Math.random() * 100) + 1;
@@ -23,14 +30,52 @@ function pickRandomNum() {
 
 function play() {
     let userValue = userInput.value;
+
+
+    if(userValue<1 || userValue>100){
+        resultArea.textContent="1과 100사이 숫자를 입력해 주세요"
+        return;
+    }
+
+    if(history.includes(userValue)){
+        resultArea.textContent="이미 입력한 숫자입니다. 다른 숫자를 입력해 주세요"
+        return;
+    }
+
+    chances--;
+    chanceArea.textContent=`남은 기회: ${chances}번`;
+    console.log("chance", chances);
+
     if(userValue < computerNum) {
-        resultArea.textContent = "UP!!!"
+        resultArea.textContent = "UP!!!";
         console.log("up!!")
     }else if(userValue > computerNum){
-        resultArea.textContent = "DOWN!!!"
+        resultArea.textContent = "DOWN!!!";
         console.log("Down!!!")
     }else{
-        resultArea.textContent = "맞췄습니다!!!"
+        resultArea.textContent = "맞췄습니다!!!";
+        gameOver=true
     }
+
+    history.push(userValue);
+    console.log(history);
+
+    if(chances < 1){
+        gameOver = true;
+    }
+
+    if(gameOver == true) {
+        playButton.disabled = true;
+
+    }
+}
+
+function reset() {
+    //user input 창이 깨끗하게 정리되고
+    userInput.value =""
+    // 새로운 번호가 생성되고
+    pickRandomNum();
+
+    resultArea.textContent="결과 값이 여기 나옵니다."
 }
 pickRandomNum();
